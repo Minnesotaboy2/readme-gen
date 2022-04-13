b8417ac9c3975132e09961d85447c834055a8563
Andrew Martinek <andrewdmartinek@gmail.com>
2022-04-12 23:43:29 -0500

    README.md generator is mostly functional, license funtctionality is a work in progress.

---
+++ README.md
@@ -0,0 +1,36 @@
+
+    #README Generater
+
+## Description
+
+You put the informartion needed for a readme into this generater and it will put it into a README.md file for you.
+
+### Installation Instructions
+
+Clone the repository, make sure you have all the files.
+
+### How to use
+
+Right click on the files in the folder, type in "node index.js" and then it will start to prompt you so all you have to do is type your response and hit the enter key.
+
+### Credits
+
+I referred to this link for what I should include in this README Generator. https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
+I also used the inquirer package. https://www.npmjs.com/package/inquirer
+
+### License
+
+Will come back to this.
+
+
+### Features
+Entire application is in the terminal. The application will prompt the user for information and saves it to a README.md file created with all of the information given.
+#### Struggles I have had with this project are...
+
+
+### Links
+https://github.com/Minnesotaboy2
+
+
+
---
+++ index.js
@@ -0,0 +1,116 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+const generatemd = ({ project, description, installation, usage, credits, license, features, struggles, github, githubRepo, deployed }) =>
+    `
+    #${project}
+
+## Description
+
+${description}
+
+### Installation Instructions
+
+${installation}
+
+### How to use
+
+${usage}
+
+### Credits
+
+${credits}
+
+### License
+
+${license}
+
+
+### Features
+${features}
+#### Struggles I have had with this project are...
+${struggles}
+
+### Links
+${github}
+
+${githubRepo}
+
+${deployed}`;
+
+inquirer
+
+    .prompt([
+        {
+            type: 'input',
+            name: 'project',
+            message: 'What is the name of the project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe the application.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the installation instructions?',
+        },       
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use this app?',
+        },        
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List any collaborators with their link, third party assets, tutorials, sources.',
+        },        
+        // {
+            // type: 'input',
+            // name: 'license',
+            // message: 'Put license information here',
+    // const licenses = [MIT, Apache, ISC],
+    // const MIT = require('./licenses/MIT'),
+    // const Apache = require('./licenses/apache'),
+    // const ISC = require('./licenses/ISC'),
+        // },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'Describe any special or unique features of this application here/',
+        },
+        {
+            type: 'input',
+            name: 'struggles',
+            message: 'Talk about any struggles you have had getting this application to work.',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Leave a link to your Github page here.',
+        },
+        {
+            type: 'input',
+            name: 'githubRepo',
+            message: 'Leave a link to this projects Github Repository',
+        },
+        {
+            type: 'input',
+            name: 'deployed',
+            message: 'Leave a link to the live site.',
+        },
+    ])
+    .then((answers) => {
+        const readmeContent = generatemd(answers);
+
+        fs.writeFile('README.md', readmeContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README')
+        );
+    })
+    // const license = ['MIT', 'Apache', 'ISC']
+    // const MIT = require('./licenses/MIT');
+    // const Apache = require('./licenses/apache')
+    // const ISC = require('./licenses/ISC')
+    // const [MIT, Apache, ISC] = license
